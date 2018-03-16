575526a58a4e31f1c5faf7a81e9d1d9efc55bbbb
liamhebert <liamhebert@gmail.com>
2018-03-16 09:25:29 -0300

    Demo,
    Feel free to look at this to know what JavaFX looks like. Plus: I found the imports that will be useful for tron

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,6 @@
 
 package sample;
+import javafx.animation.Animation;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
@@ -8,25 +9,39 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import javafx.scene.shape.Circle;
-
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
+import javafx.util.Duration;
 import java.awt.*;
-
+import javafx.scene.shape.Rectangle;
+//WE ARE GONNA USE THESE THREE BIG TIME
+import javafx.scene.shape.Path;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.LineTo;
+import javafx.util.Duration;
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-      primaryStage.setTitle("ur mum gay");
-      Circle circle = new Circle();
-      circle.setCenterX(250);
-      circle.setCenterY(250);
-      circle.setRadius(100);
-      circle.setFill(Paint.valueOf("RED"));
-      Group root = new Group(circle);
-      Scene scene = new Scene (root, 500,500);
+      primaryStage.setTitle("gaymom");
+      Circle circ = new Circle();
+      circ.setCenterY(250);
+      circ.setCenterX(450);
+      circ.setRadius(100);
+      circ.setFill(Paint.valueOf("RED"));
+      TranslateTransition translateTransition = new TranslateTransition();
+      translateTransition.setNode(circ);
+      translateTransition.setToY(300);
+      translateTransition.setToX(200);
+      translateTransition.setDuration(Duration.millis(100));
+      translateTransition.setCycleCount(10);
+      translateTransition.setAutoReverse(true);
+      translateTransition.play();
+      Group root = new Group(circ);
+      Scene scene = new Scene (root, 900,500);
+      scene.setFill(Paint.valueOf("WHITE"));
       primaryStage.setScene(scene);
       primaryStage.show();
-
-
     }
 
 
