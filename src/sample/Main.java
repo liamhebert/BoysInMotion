59e2623191e231212cbf9d466d223ac1b286f1a3
liamhebert <liamhebert@gmail.com>
2018-03-19 23:25:04 -0300

    GUI 1.0 - added results and credits screen.
    Early version of the GUI. Might need some tuning for logic but it should be good.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,25 +1,21 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.shape.Rectangle;
-import javafx.animation.ScaleTransition;
-import javafx.animation.PathTransition;
 import javafx.scene.shape.Path;
 import javafx.scene.shape.LineTo;
-import javafx.scene.shape.Line;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.text.*;
 
@@ -35,36 +31,46 @@ public class Main extends Application {
     private static Font subTitle;
     private static Font subFont = new Font("Consolas", 37.333333 * 1.5);
     private static Font smallFont = new Font ("Consolas", 30);
+    private static Font popFont;
+    private static Color p1;
+    private static Color p2;
+
     public void start(Stage primaryStage) throws Exception{
         titleFont = Font.loadFont(new FileInputStream("resources/LeagueSpartan.ttf"),106.66667 * 1.5);
         subTitle = Font.loadFont(new FileInputStream("resources/LeagueSpartan.ttf"),42.6666667 * 1.5);
+        popFont = Font.loadFont(new FileInputStream("resources/LeagueSpartan.ttf"),30);
+        p1 = Color.rgb(255,0,0);
+        p2 = Color.rgb(0,176,240);
         primaryStage.setTitle("Loading...");
+        primaryStage.setResizable(false);
         primaryStage.setScene(customizeBike());
-        primaryStage.setTitle("customizeBike");
+        //primaryStage.setScene(results("Liam", p2)); //second variable lets the screen know who won
+        //primaryStage.setScene(credits());
         primaryStage.show();
+        primaryStage.setTitle("results");
         Thread.sleep(1000);
-        int input = 0;
-        System.out.println("1 : Customise bikes, 2: Game Screen, 3: Main, 4: quit");
-        Scanner kb = new Scanner(System.in);
-        input = kb.nextInt();
-        if (input == 1) {
-            primaryStage.setTitle("Loading...");
-            primaryStage.setScene(customizeBike());
-            primaryStage.setTitle("Customize Bike");
-            primaryStage.show();
-        }
-        else if (input == 2) {
-            primaryStage.setTitle("Loading...");
-            //primaryStage.setScene(game());
-            primaryStage.setTitle("Game");
-            primaryStage.show();
-        }
-        else if (input == 3) {
-            primaryStage.setTitle("Loading...");
-            primaryStage.setScene(mainMenu());
-            primaryStage.setTitle("Main Menu");
-            primaryStage.show();
-        }
+        int input;
+        //System.out.println("1 : Customise bikes, 2: Game Screen, 3: Main, 4: quit");
+        //Scanner kb = new Scanner(System.in);
+        //input = kb.nextInt();
+//        if (input == 1) {
+//            primaryStage.setTitle("Loading...");
+//            primaryStage.setScene(customizeBike());
+//            primaryStage.setTitle("Customize Bike");
+//            primaryStage.show();
+//        }
+//        else if (input == 2) {
+//            primaryStage.setTitle("Loading...");
+//            //primaryStage.setScene(game());
+//            primaryStage.setTitle("Game");
+//            primaryStage.show();
+//        }
+//        else if (input == 3) {
+//            primaryStage.setTitle("Loading...");
+//            primaryStage.setScene(mainMenu());
+//            primaryStage.setTitle("Main Menu");
+//            primaryStage.show();
+//        }
     }
     public Scene mainMenu() {
         Text title = new Text("Tron.");
@@ -90,12 +96,11 @@ public class Main extends Application {
         layout.setLayoutY(150);
 
         Group root = new Group(layout);
-        Scene mainMenu = new Scene (root,800,800);
-        mainMenu.setFill(Paint.valueOf("BLACK"));
-        return mainMenu;
+        return new Scene (root,800,800, Paint.valueOf("BLACK"));
     }
 
     public Scene customizeBike() {
+        Group root = new Group();
         VBox layout = new VBox(40);
         layout.setLayoutY(150);
         layout.setLayoutX(50);
@@ -103,7 +108,12 @@ public class Main extends Application {
         title.setFont(subTitle);
         title.setFill(Paint.valueOf("WHITE"));
         layout.getChildren().add(title);
-
+        Rectangle selectedColorP1 = new Rectangle(812,375 - 250);
+        selectedColorP1.setFill(p1);
+        selectedColorP1.setY(250);
+        Rectangle selectedColorP2 = new Rectangle(812,375-250);
+        selectedColorP2.setFill(p2);
+        selectedColorP2.setY(375);
         layout.getChildren().addAll(playerRow("player 1 :", 0), playerRow("player 2 :", 2));
 
         Rectangle startButton = new Rectangle(600, 125);
@@ -115,22 +125,22 @@ public class Main extends Application {
         buttonGo.setFill(Paint.valueOf("WHITE"));
         buttonGo.setX(270);
         buttonGo.setY(650);
-        Group root = new Group(layout, buttonGo);
+        root.getChildren().addAll(selectedColorP1,selectedColorP2,layout, buttonGo);
         return new Scene(root, 800, 800, Paint.valueOf("BLACK"));
     }
     private HBox playerRow(String player, int color){
         HBox player1 = new HBox(20);
-        Text p1 = new Text(player);
-        p1.setFont(smallFont);
-        p1.setFill(Paint.valueOf("WHITE"));
-        player1.getChildren().add(p1);
+        Text pT = new Text(player);
+        pT.setFont(smallFont);
+        pT.setFill(Paint.valueOf("WHITE"));
+        player1.getChildren().add(pT);
 
         VBox inputAndColor = new VBox(10);
         TextField p1NameInput = new TextField("Name");
         inputAndColor.getChildren().add(p1NameInput);
 
         HBox colors = new HBox(10);
-        Paint[] allColors = {Paint.valueOf("RED"), Paint.valueOf("ORANGE"), Paint.valueOf("BLUE"), Paint.valueOf("GREEN"), Paint.valueOf("Purple")};
+        Paint[] allColors = {p1, Paint.valueOf("ORANGE"), p2, Paint.valueOf("GREEN"), Paint.valueOf("Purple")}; //p1 and p2 are only there for demo purposes
         Rectangle color1 = new Rectangle(50,50);
         color1.setFill(allColors[0]);
         Rectangle color2 = new Rectangle(50,50);
@@ -145,12 +155,85 @@ public class Main extends Application {
         inputAndColor.getChildren().add(colors);
         player1.getChildren().add(inputAndColor);
 
-        Rectangle selectedColor = new Rectangle(85,85);
-        selectedColor.setFill(allColors[color]);
-        player1.getChildren().add(selectedColor);
 
-        HBox.setMargin(selectedColor, new Insets(0,0,0,20));
+        //make sure that Paint p1/p2 equal the selected color;
         return player1;
+    }
+
+    public Scene results(String Pwinner, Color winnerC){
+        Group root = new Group();
+        Rectangle colorWinner = new Rectangle(0,0,810,280);
+        colorWinner.setFill(winnerC); //color of the winner
+        root.getChildren().add(colorWinner);
+        VBox display = new VBox(20);
+        display.setLayoutX(50);
+        display.setLayoutY(200);
+        root.getChildren().add(display);
+
+        Text winner = new Text(Pwinner + " Wins!");
+        winner.setFont(subTitle);
+        winner.setFill(Paint.valueOf("WHITE"));
+        display.getChildren().add(winner);
+
+        HBox score = scoreDisplay(1,3);
+        display.getChildren().add(score);
+
+        Text playAgain = new Text("Play again?");
+        playAgain.setFont(popFont);
+        playAgain.setFill(Paint.valueOf("WHITE"));
+        VBox.setMargin(playAgain, new Insets(50,0,0,0));
+        display.getChildren().add(playAgain);
+
+        HBox options = new HBox(30);
+        Text yes = new Text("yes");
+        yes.setFont(smallFont);
+        yes.setFill(Paint.valueOf("WHITE"));
+        Text no = new Text("no");
+        no.setFont(smallFont);
+        no.setFill(Paint.valueOf("WHITE"));
+        options.getChildren().addAll(yes,no);
+        display.getChildren().add(options);
+
+        return new Scene(root, 800,800, Paint.valueOf("BLACK"));
+    }
+
+    public Scene credits(){
+        Group root = new Group();
+        VBox layout = new VBox(30);
+        layout.setLayoutX(50);
+        layout.setLayoutY(200);
+        Text thanksTitle = new Text("Thanks for Playing!");
+        thanksTitle.setFont(subTitle);
+        thanksTitle.setFill(Paint.valueOf("WHITE"));
+        Text theBoys = new Text("The Boys in Motion:\n" +
+                "Liam Hebert\n" +
+                "Arjav Gupta\n" +
+                "Bishal Dasputa\n" +
+                "Logan Sutherland\n" +
+                "Kenzie Dowie\n");
+        theBoys.setFill(Paint.valueOf("WHITE"));
+        theBoys.setFont(smallFont);
+        Text fontUsed = new Text("Title Font: League Spartan - The League of Moveable Type");
+        fontUsed.setFill(Paint.valueOf("WHITE"));
+        fontUsed.setFont(smallFont);
+        layout.getChildren().addAll(thanksTitle, theBoys,fontUsed);
+        root.getChildren().add(layout);
+        return new Scene(root,1200,800,Paint.valueOf("BLACK"));
+    }
+
+    public HBox scoreDisplay(int p1S, int p2S){
+        Text scoreP1 = new Text(p1S + "");
+        scoreP1.setFill(p1);
+        scoreP1.setFont(popFont);
+        Text scoreP2 = new Text(p2S + "");
+        scoreP2.setFill(p2);
+        scoreP2.setFont(popFont);
+        Text dash = new Text(" - ");
+        dash.setFill(Paint.valueOf("WHITE"));
+        dash.setFont(popFont);
+        HBox display = new HBox(5);
+        display.getChildren().addAll(scoreP1,dash,scoreP2);
+        return display;
     }
     public static void main(String[] args) {
         launch(args);
